24dca196ba9a750b4b2577d3020fd860e6ae9c48
paskuvan <majopaskuvan@gmail.com>
2026-01-07 20:53:24 -0300

    add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
 const page = () => {
   return (
-    <div>page</div>
+    <div className="p-6 bg-gray-100 border border-gray-300 rounded-lg shadow-lg text-gray-700 text-lg font-semibold leading-relaxed tracking-wide text-center max-w-sm mx-auto mt-10">
+      Welcome to the Next.js App!
+    </div>
   )
 }
 
