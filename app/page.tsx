f1b1cf6f9b751d985eb2ba4803a6c1eb152ccbfc
paskuvan <majopaskuvan@gmail.com>
2026-01-08 11:11:29 -0300

    add fonts and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-const page = () => {
+const Home = () => {
   return (
-    <div className="p-6 bg-gray-100 border border-gray-300 rounded-lg shadow-lg text-gray-700 text-lg font-semibold leading-relaxed tracking-wide text-center max-w-sm mx-auto mt-10">
-      Welcome to the Next.js App!
-    </div>
+  <h1 className="h1-bold font-inter">Welcome to the world of Next</h1>
+ 
   )
 }
 
-export default page
+export default Home
