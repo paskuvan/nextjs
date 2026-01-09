077796955305b7b03bc214075b48175008988e46
paskuvan <majopaskuvan@gmail.com>
2026-01-09 00:03:20 -0300

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-const Home = () => {
-  return (
-  <h1 className="h1-bold font-inter">Welcome to the world of Next</h1>
- 
-  )
-}
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    
+  </>
+);
 
 export default Home
