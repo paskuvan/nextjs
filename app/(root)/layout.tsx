b78e59b044a321ec2de3bc89417d58a93986d656
paskuvan <majopaskuvan@gmail.com>
2026-01-10 11:54:12 -0300

    devicons

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Navbar from "@/components/navigation";
 import LeftSidebar from "@/components/navigation/LeftSidebar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return <main className="background-light850_dark100 realtive">
@@ -11,6 +12,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
         <div className="mx-auto w-full max-w-5xl">{children}</div>
       </section>
+
+      <RightSidebar />
     </div>
     
   </main>
