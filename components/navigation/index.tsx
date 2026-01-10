5238632900fa49ea8df342d69badb033d56c9adf
paskuvan <majopaskuvan@gmail.com>
2026-01-10 00:50:49 -0300

    add more folders

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 import Theme from './Theme'
+import MobileNavigation from './MobileNavigation';
 
 
 const Navbar = () => {
@@ -25,6 +26,8 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
        <Theme />
+
+       <MobileNavigation />
       </div>
     </nav>
   
