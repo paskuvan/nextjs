d1a8a5dc4777109ca8b0f6cd7dcf8a9b3db5858e
paskuvan <majopaskuvan@gmail.com>
2026-01-12 23:08:52 -0300

    route handlers

---
+++ app/database.ts
@@ -0,0 +1,64 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix Next.js API Route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Add dark mode",
+    status: "closed",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "Improve performance",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 4,
+    name: "Implement SEO and Meta tags",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 5,
+    name: "Fix deployment issue",
+    status: "closed",
+    type: "bug",
+  },
+  {
+    id: 6,
+    name: "Update dependencies",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 7,
+    name: "Refactor codebase",
+    status: "closed",
+    type: "bug",
+  },
+  {
+    id: 8,
+    name: "Add unit tests",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 9,
+    name: "Fix CSS styling issues",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 10,
+    name: "Optimize images",
+    status: "closed",
+    type: "feature",
+  }
+];
+
+export default tickets;
