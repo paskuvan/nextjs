3a461f5e23e4b42b8a5736c47e8212e74115b949
paskuvan <majopaskuvan@gmail.com>
2026-01-14 13:14:58 -0300

    auth logout

--- components/navigation/MobileNavigation.tsx
+++ components/navigation/MobileNavigation.tsx
@@ -12,61 +12,91 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import ROUTES from "@/constants/routes"
-const MobileNavigation = () => {
+import { auth } from "@/auth"
+
+const MobileNavigation = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+
   return (
     <Sheet>
-  <SheetTrigger asChild>
-    <Image 
-    src="icons/hamburger.svg"
-    width={36}
-    height={36}
-    alt="Menu"
-    className="invert-colors sm:hidden"
-    />
-    </SheetTrigger>
-  <SheetContent 
-  side="left"
-  className="background-light900_dark200 border-none">
-     <SheetTitle className="hidden">Navigation</SheetTitle>
-     <Link href="/" className="flex items-center gap-1">
-     <Image src="/images/site-logo.svg"
-     width={23}
-     height={23}
-     alt="DevFlow Logo"
-     />
-     <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
-          Dev<span className="text-primary-500">Flow</span>
-        </p>
-     </Link>
+      <SheetTrigger asChild>
+        <Image
+          src="/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="Menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <SheetTitle className="hidden">Navigation</SheetTitle>
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Logo"
+          />
 
-     <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
-        <SheetClose asChild>
+          <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
+            Dev<span className="text-primary-500">Flow</span>
+          </p>
+        </Link>
+
+        <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+          <SheetClose asChild>
             <section className="flex h-full flex-col gap-6 pt-16">
-            <NavLinks isMobileNav />
+              <NavLinks isMobileNav />
             </section>
-        </SheetClose>
+          </SheetClose>
 
-        <div className="flex flex-col gap-3">
-            <SheetClose asChild>
-                <Link href={ROUTES.SIGN_IN}>
-                <Button className="small-medium btn-secondary min-h-10.25 w-full rounded-lg px-4 py-3 shadow-none">
-                    <span className="primary-text-gradient"> Log In</span>
-                </Button>
-                </Link>
-            </SheetClose>
+          <div className="flex flex-col gap-3">
+            {userId ? (
+              <SheetClose asChild>
+                <form
+                  action={async () => {
+                    "use server";
 
-             <SheetClose asChild>
-              <Link href={ROUTES.SIGN_UP}>
-                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-10.25 w-full rounded-lg border px-4 py-3 shadow-none">
-                  Sign Up
-                </Button>
-              </Link>
-            </SheetClose>
+                    await signOut();
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    className="base-medium w-fit !bg-transparent px-4 py-3"
+                  >
+                    <LogOut className="size-5 text-black dark:text-white" />
+                    <span className="text-dark300_light900">Logout</span>
+                  </Button>
+                </form>
+              </SheetClose>
+            ) : (
+              <>
+                <SheetClose asChild>
+                  <Link href={ROUTES.SIGN_IN}>
+                    <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                      <span className="primary-text-gradient">Log In</span>
+                    </Button>
+                  </Link>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Link href={ROUTES.SIGN_UP}>
+                    <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                      Sign Up
+                    </Button>
+                  </Link>
+                </SheetClose>
+              </>
+            )}
+          </div>
         </div>
-     </div>
-  </SheetContent>
-</Sheet>
-  )
-}
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default MobileNavigation
--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -4,11 +4,14 @@ import React from 'react'
 
 import Theme from './Theme'
 import MobileNavigation from './MobileNavigation';
+import { auth } from '@/auth';
 
 
-const Navbar = () => {
-  
-    return <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
@@ -25,12 +28,20 @@ const Navbar = () => {
       <p>Global Search</p>
 
       <div className="flex-between gap-5">
-       <Theme />
+        <Theme />
 
-       <MobileNavigation />
+        {session?.user?.id && (
+          <UserAvatar
+            id={session.user.id}
+            name={session.user.name!}
+            imageUrl={session.user?.image}
+          />
+        )}
+
+        <MobileNavigation />
       </div>
     </nav>
-  
+  );
 };
 
 export default Navbar;
