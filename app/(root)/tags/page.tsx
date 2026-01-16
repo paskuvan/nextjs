fdb5bf83a3b38163269db19c0f0ebe96a3f5d550
paskuvan <majopaskuvan@gmail.com>
2026-01-16 12:15:03 -0300

    tags action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { getTags } from "@/lib/actions/tag.actions";
 
-const Tags = () => {
-  return (
-    <div>Tags</div>
-  )
-}
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "javascript",
+  });
 
-export default Tags
+  const { tags } = data || {};
+
+  console.log("TAGS", JSON.stringify(tags, null, 2));
+
+  return <div>Tags</div>;
+};
+
+export default Tags;
